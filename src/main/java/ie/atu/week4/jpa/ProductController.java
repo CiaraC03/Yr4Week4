c39c53b1430c64c9c1c37da2fec746b0d2a03a94
ciara <g00402357@atu.ie>
2024-10-14 12:39:45 +0100

    Updated controller and service class

--- src/main/java/ie/atu/week4/jpa/ProductController.java
+++ src/main/java/ie/atu/week4/jpa/ProductController.java
@@ -25,38 +25,16 @@ public class ProductController {
         return ResponseEntity.ok(productList);
     }
 
-    private Product findProductById(int id) {
-        for (Product product : productList) {
-            if (product.getProductCode() == id) {
-                return product;
-            }
-        }
-        return null;
-    }
 
     @PutMapping("/updateProduct/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            existingProduct.setProductName(updatedProduct.getProductName());
-            existingProduct.setProductDescription(updatedProduct.getProductDescription());
-            existingProduct.setProductPrice(updatedProduct.getProductPrice());
-            return ResponseEntity.ok(existingProduct);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        Product update = productService.updateProduct(id, updatedProduct);
+        return ResponseEntity.ok(update);
     }
 
     @DeleteMapping("/deleteProduct/{id}")
     public ResponseEntity<List<Product>> deleteProduct(@PathVariable int id) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            productList.remove(existingProduct);
-            return ResponseEntity.ok(productList);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+       List<Product> productList = productService.deleteProduct(id);
+       return ResponseEntity.ok(productList);
     }
 }
